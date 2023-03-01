39427d038e5c42d94cfa0262e8166cdcb259a900
Ahmed <ahmedthecat3457@gmail.com>
2023-03-01 19:24:25 +0300

    Removed unnecessary files

--- main.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
-set PATH=c:\users\user\appdata\local\programs\python\python38;%PATH%
-set PYTHONHOME=C:\Users\user\AppData\Local\Programs\Python\Python38
-"%~dp0.\main.exe" %*
